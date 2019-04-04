dc97b8c1c75bb79b7e17a2670fb6578c5adf70ab
Leonard <leonard.sellem@gmail.com>
2019-04-04 18:09:11 +0200

    create some lists

---
+++ script.py
@@ -0,0 +1,7 @@
+last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("architecture", 65)]
+
+#Create some lists
+subjects = ["physics", "calculus", "poetry", "history"]
+grades = [98, 97, 85, 88]
+gradebook = list(zip(subjects, grades))
+print(gradebook)
