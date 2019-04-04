9f3c2b8f44f7489f0ed1e08b8a714b2033463ee7
Leonard <leonard.sellem@gmail.com>
2019-04-04 18:14:40 +0200

    complete project

--- script.py
+++ script.py
@@ -3,5 +3,12 @@ last_semester_gradebook = [("politics", 80), ("latin", 96), ("dance", 97), ("arc
 #Create some lists
 subjects = ["physics", "calculus", "poetry", "history"]
 grades = [98, 97, 85, 88]
+subjects.append("computer science")
+grades.append(100)
 gradebook = list(zip(subjects, grades))
+gradebook.append(("visual arts", 93))
 print(gradebook)
+
+full_gradebook = gradebook + last_semester_gradebook
+
+print(full_gradebook)
